919fe58c906c78cd283791df7111855df2c665bf
David <davidthemaster30@hotmail.com>
2024-11-22 00:24:40 -0500

    Make work with latest BepInEx (725) and ConfigurationManager

--- FurnitureChanges.cs
+++ FurnitureChanges.cs
@@ -1,9 +1,11 @@
+using BepInEx.Configuration;
 using HarmonyLib;
 using UnityEngine;
 
 namespace FurnitureMod;
 
-public partial class FurnitureMod
+[HarmonyPatch]
+public static class FurnitureChangesPatch
 {
     internal static bool shadowIgnore = false;
     internal static GameObject newButton;
@@ -12,113 +14,131 @@ public partial class FurnitureMod
     internal const float gridMax = 1f;
     internal const int NinetyDegrees = 90;
 
-    [HarmonyPatch]
-    public static class FurnitureChanges
+    internal static ConfigEntry<bool> EnableFurnitureSnapping;
+    internal static ConfigEntry<bool> EnableAlwaysIgnoreCollison;
+    internal static ConfigEntry<KeyboardShortcut> ToggleKey;
+    internal static ConfigEntry<RF5Input.Key> ToggleButton;
+    internal static ConfigEntry<KeyboardShortcut> IncreaseGridKey;
+    internal static ConfigEntry<KeyboardShortcut> DecreaseGridKey;
+
+    private static readonly uint[] ButtonList = new List<uint>((uint[])Enum.GetValues(typeof(RF5Input.Key))).Where(x => x != (uint)RF5Input.Key.ANYKEY || x != (uint)RF5Input.Key.ALLKEY).ToArray();
+    private static readonly ConfigDescription ToggleButtonDescription = new ConfigDescription(
+    "Set the Button you wish to press while Holding Furniture to Ignore Placement Collison (Not required if AlwaysIgnore is true)",
+    new AcceptableValueList<uint>(ButtonList));
+    internal static void LoadConfig(ConfigFile Config)
     {
-        private static void CreateIgnoreBtn()
-        {
-            if (newButton is null)
-            {
-                newButton = GameObject.Instantiate(GameObject.Find("B"), new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0), GameObject.Find("OnHandUI").transform);
-                newButton.name = "ShadowIgnoreBtn";
-                newButton.GetComponent<RectTransform>().anchoredPosition = GameObject.Find("B").GetComponent<RectTransform>().anchoredPosition + new Vector2(60, 80);
-                newButton.GetComponent<RectTransform>().sizeDelta = new Vector2(400, 103);
-                newButton.GetChild(0).GetComponent<RectTransform>().anchoredPosition += new Vector2(-57.7001f, 0);
-                newButton.GetChild(1).GetComponent<RectTransform>().sizeDelta = new Vector2(400, 30);
-                int testKeyImage = Convert.ToInt32(Enum.Parse(typeof(ButtonSpriteManager.KeyImageType), kToggleButton.Value.ToString()));
+        EnableAlwaysIgnoreCollison = Config.Bind("Always Ignore Placement Collison", "AlwaysIgnore", false, "Set to true to always Ignore Furniture Placement Collison");
+        EnableFurnitureSnapping = Config.Bind("Furniture Snap to Grid", "SnapToGrid", true, "Set to true to snap furniture to a grid");
+        ToggleKey = Config.Bind("Key to Ignore Placement", "IgnoreKey", new KeyboardShortcut(KeyCode.LeftShift), "Set the Key you wish to press while Holding Furniture to Ignore Placement Collison (Not required if AlwaysIgnore is true)");
+        ToggleButton = Config.Bind("Button to Ignore Placement", "IgnoreButton", RF5Input.Key.R, ToggleButtonDescription);
+        IncreaseGridKey = Config.Bind("Button to Increase Grid Size", "IncreaseGrid", new KeyboardShortcut(KeyCode.Period), "Set the Key you wish to press to Increase grid size");
+        DecreaseGridKey = Config.Bind("Button to Decrease Grid Size", "DecreaseGrid", new KeyboardShortcut(KeyCode.Comma), "Set the Key you wish to press to Decrease grid size");
+    }
 
-                newButton.GetChild(0).GetComponent<ButtonImageController>().nowButtonType = (ButtonSpriteManager.KeyImageType)testKeyImage;
-                newButton.GetChild(0).GetComponent<ButtonImageController>().startButtonType = (ButtonSpriteManager.KeyImageType)testKeyImage;
-                newButton.GetChild(0).GetComponent<ButtonImageController>().RefreshImage();
-                newButton.GetComponentInChildren<SText>().text = "IGNORE BLOCK";
-            }
+    private static void CreateIgnoreBtn()
+    {
+        if (newButton is null)
+        {
+            newButton = GameObject.Instantiate(GameObject.Find("B"), new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0), GameObject.Find("OnHandUI").transform);
+            newButton.name = "ShadowIgnoreBtn";
+            newButton.GetComponent<RectTransform>().anchoredPosition = GameObject.Find("B").GetComponent<RectTransform>().anchoredPosition + new Vector2(60, 80);
+            newButton.GetComponent<RectTransform>().sizeDelta = new Vector2(400, 103);
+            newButton.GetChild(0).GetComponent<RectTransform>().anchoredPosition += new Vector2(-57.7001f, 0);
+            newButton.GetChild(1).GetComponent<RectTransform>().sizeDelta = new Vector2(400, 30);
+            int testKeyImage = Convert.ToInt32(Enum.Parse(typeof(ButtonSpriteManager.KeyImageType), ToggleButton.Value.ToString()));
+
+            newButton.GetChild(0).GetComponent<ButtonImageController>().nowButtonType = (ButtonSpriteManager.KeyImageType)testKeyImage;
+            newButton.GetChild(0).GetComponent<ButtonImageController>().startButtonType = (ButtonSpriteManager.KeyImageType)testKeyImage;
+            newButton.GetChild(0).GetComponent<ButtonImageController>().RefreshImage();
+            newButton.GetComponentInChildren<SText>().text = "IGNORE BLOCK";
         }
+    }
 
-        [HarmonyPatch(typeof(ItemFurniture.PlayerItemFurniture2), nameof(ItemFurniture.PlayerItemFurniture2.Update))]
-        [HarmonyPostfix]
-        public static void Update(ItemFurniture.PlayerItemFurniture2 __instance)
+    [HarmonyPatch(typeof(ItemFurniture.PlayerItemFurniture2), nameof(ItemFurniture.PlayerItemFurniture2.Update))]
+    [HarmonyPostfix]
+    public static void Update(ItemFurniture.PlayerItemFurniture2 __instance)
+    {
+        if (__instance.CurrentState == ItemFurniture.PlayerItemFurniture2.State.Hold)
         {
-            if (__instance.CurrentState == ItemFurniture.PlayerItemFurniture2.State.Hold)
+            if (GameObject.Find("B") is not null)
             {
-                if (GameObject.Find("B") is not null)
-                {
-                    CreateIgnoreBtn();
-                }
+                CreateIgnoreBtn();
+            }
 
-                if (bAlwaysIgnore.Value)
+            if (EnableAlwaysIgnoreCollison.Value)
+            {
+                shadowIgnore = true;
+            }
+            else
+            {
+                if (ToggleKey.Value.IsDown() || RF5Input.Pad.Edge(ToggleButton.Value))
                 {
                     shadowIgnore = true;
                 }
-                else
+
+                if (ToggleKey.Value.IsUp() || RF5Input.Pad.End(ToggleButton.Value))
                 {
-                    if (UnityEngine.Input.GetKeyDown(kToggleKey.Value) || RF5Input.Pad.Edge(kToggleButton.Value))
-                    {
-                        shadowIgnore = true;
-                    }
-
-                    if (UnityEngine.Input.GetKeyUp(kToggleKey.Value) || RF5Input.Pad.End(kToggleButton.Value))
-                    {
-                        shadowIgnore = false;
-                    }
-                }
-
-                if (UnityEngine.Input.GetKeyDown(kIncreaseGrid.Value))
-                {
-                    gridSize = Mathf.Clamp(gridSize + gridIncrement, gridIncrement, gridMax);
-                }
-                if (UnityEngine.Input.GetKeyUp(kDecreaseGrid.Value))
-                {
-                    gridSize = Mathf.Clamp(gridSize - gridIncrement, gridIncrement, gridMax);
-                }
-            }
-        }
-
-        [HarmonyPatch(typeof(ItemFurniture.FurnitureShadow), nameof(ItemFurniture.FurnitureShadow.updateDisp))]
-        [HarmonyPostfix]
-        public static void updateDisp(ItemFurniture.FurnitureShadow __instance)
-        {
-            if (__instance.isActive && shadowIgnore)
-            {
-                __instance._materials[0].SetColor("_Color", new Color(1, 1, 1, 0.5f));
-            }
-        }
-
-        [HarmonyPatch(typeof(ItemFurniture.FurnitureShadow), nameof(ItemFurniture.FurnitureShadow.CheckHit))]
-        [HarmonyPrefix]
-        public static bool CheckHit(ItemFurniture.FurnitureShadow __instance)
-        {
-            if (__instance.isActive && shadowIgnore)
-            {
-                Vector3 oldLocation = __instance.transform.position;
-                Vector3 oldRotation = __instance.transform.eulerAngles;
-
-                __instance.isHit = !shadowIgnore;
-                if (bFurnitureSnapping.Value)
-                {
-                    __instance.transform.position = new Vector3(Mathf.Round(oldLocation.x / gridSize) * gridSize, oldLocation.y, Mathf.Round(oldLocation.z / gridSize) * gridSize);
-                    __instance.transform.eulerAngles = new Vector3(oldRotation.x, Mathf.Round(oldRotation.y / NinetyDegrees) * NinetyDegrees, oldRotation.z);
+                    shadowIgnore = false;
                 }
             }
 
-            return !shadowIgnore;
-        }
-
-        [HarmonyPatch(typeof(ItemFurniture.PlayerItemFurniture2), nameof(ItemFurniture.PlayerItemFurniture2.OnPutOn))]
-        [HarmonyPrefix]
-        public static void OnPutOn(ItemFurniture.PlayerItemFurniture2 __instance)
-        {
-            if (bFurnitureSnapping.Value)
+            if (IncreaseGridKey.Value.IsDown())
             {
-                Vector3 oldLocation = __instance.transform.position;
-                Vector3 oldRotation = __instance.transform.eulerAngles;
+                gridSize = Mathf.Clamp(gridSize + gridIncrement, gridIncrement, gridMax);
+            }
+
+            if (DecreaseGridKey.Value.IsDown())
+            {
+                gridSize = Mathf.Clamp(gridSize - gridIncrement, gridIncrement, gridMax);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(ItemFurniture.FurnitureShadow), nameof(ItemFurniture.FurnitureShadow.updateDisp))]
+    [HarmonyPostfix]
+    public static void updateDisp(ItemFurniture.FurnitureShadow __instance)
+    {
+        if (__instance.isActive && shadowIgnore)
+        {
+            __instance._materials[0].SetColor("_Color", new Color(1, 1, 1, 0.5f));
+        }
+    }
+
+    [HarmonyPatch(typeof(ItemFurniture.FurnitureShadow), nameof(ItemFurniture.FurnitureShadow.CheckHit))]
+    [HarmonyPrefix]
+    public static bool CheckHit(ItemFurniture.FurnitureShadow __instance)
+    {
+        if (__instance.isActive && shadowIgnore)
+        {
+            Vector3 oldLocation = __instance.transform.position;
+            Vector3 oldRotation = __instance.transform.eulerAngles;
+
+            __instance.isHit = !shadowIgnore;
+            if (EnableFurnitureSnapping.Value)
+            {
                 __instance.transform.position = new Vector3(Mathf.Round(oldLocation.x / gridSize) * gridSize, oldLocation.y, Mathf.Round(oldLocation.z / gridSize) * gridSize);
                 __instance.transform.eulerAngles = new Vector3(oldRotation.x, Mathf.Round(oldRotation.y / NinetyDegrees) * NinetyDegrees, oldRotation.z);
             }
+        }
 
-            if (newButton is not null)
-            {
-                GameObject.Destroy(newButton);
-            }
+        return !shadowIgnore;
+    }
+
+    [HarmonyPatch(typeof(ItemFurniture.PlayerItemFurniture2), nameof(ItemFurniture.PlayerItemFurniture2.OnPutOn))]
+    [HarmonyPrefix]
+    public static void OnPutOn(ItemFurniture.PlayerItemFurniture2 __instance)
+    {
+        if (EnableFurnitureSnapping.Value)
+        {
+            Vector3 oldLocation = __instance.transform.position;
+            Vector3 oldRotation = __instance.transform.eulerAngles;
+            __instance.transform.position = new Vector3(Mathf.Round(oldLocation.x / gridSize) * gridSize, oldLocation.y, Mathf.Round(oldLocation.z / gridSize) * gridSize);
+            __instance.transform.eulerAngles = new Vector3(oldRotation.x, Mathf.Round(oldRotation.y / NinetyDegrees) * NinetyDegrees, oldRotation.z);
+        }
+
+        if (newButton is not null)
+        {
+            GameObject.Destroy(newButton);
         }
     }
 }
--- FurniturePlacement.cs
+++ FurniturePlacement.cs
@@ -1,6 +1,6 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
-using BepInEx.IL2CPP;
+using BepInEx.Unity.IL2CPP;
 using BepInEx.Logging;
 using HarmonyLib;
 using UnityEngine;
@@ -9,7 +9,7 @@ namespace FurnitureMod;
 
 [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
 [BepInProcess(GameProcessName)]
-public partial class FurnitureMod : BasePlugin
+public class FurnitureModPlugin : BasePlugin
 {
     #region PluginInfo
     internal const string pluginGuid = "BetterFurniturePlacementMod";
@@ -20,25 +20,12 @@ public partial class FurnitureMod : BasePlugin
 
     internal static new ManualLogSource Log = BepInEx.Logging.Logger.CreateLogSource("FurnitureMod");
 
-    internal static ConfigEntry<bool> bFurnitureSnapping;
-    internal static ConfigEntry<bool> bAlwaysIgnore;
-    internal static ConfigEntry<KeyCode> kToggleKey;
-    internal static ConfigEntry<RF5Input.Key> kToggleButton;
-    internal static ConfigEntry<KeyCode> kIncreaseGrid;
-    internal static ConfigEntry<KeyCode> kDecreaseGrid;
-
     public override void Load()
     {
-        Log = base.Log;
         Log.LogInfo($"Plugin {pluginGuid} is loaded!");
 
-        bAlwaysIgnore = Config.Bind("Always Ignore Placement Collison", "AlwaysIgnore", false, "Set to true to always Ignore Furniture Placement Collison");
-        bFurnitureSnapping = Config.Bind("Furniture Snap to Grid", "SnapToGrid", true, "Set to true to snap furniture to a grid");
-        kToggleKey = Config.Bind("Key to Ignore Placement", "IgnoreKey", KeyCode.LeftShift, "Set the Key you wish to press while Holding Furniture to Ignore Placement Collison (Not required if AlwaysIgnore is true)");
-        kToggleButton = Config.Bind("Button to Ignore Placement", "IgnoreButton", RF5Input.Key.R, "Set the Button you wish to press while Holding Furniture to Ignore Placement Collison (Not required if AlwaysIgnore is true)");
-        kIncreaseGrid = Config.Bind("Button to Increase Grid Size", "IncreaseGrid", KeyCode.Period, "Set the Key you wish to press to Increase grid size");
-        kDecreaseGrid = Config.Bind("Button to Decrease Grid Size", "DecreaseGrid", KeyCode.Comma, "Set the Key you wish to press to Decrease grid size");
+        FurnitureChangesPatch.LoadConfig(Config);
 
-        Harmony.CreateAndPatchAll(typeof(FurnitureChanges));
+        Harmony.CreateAndPatchAll(typeof(FurnitureChangesPatch));
     }
 }
